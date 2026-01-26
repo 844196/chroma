a2999d7afd89d587c8364f351fed7c7d9c933321
844196 <844196@users.noreply.github.com>
2026-01-26 22:50:49 +0900

    chore(build): prepare bytecode builds

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -5,59 +5,61 @@ import { ChromeClient } from './externals/chrome-client.ts'
 import { Config } from './externals/config.ts'
 import { ChromeService } from './services/chrome-service.ts'
 
-const {
-  args: parsedArgs,
-  literal: parsedLiteral,
-  options: parsedOpts,
-} = await new Command()
-  .name('chroma')
-  .description('Open URL in specific Google Chrome profile.')
-  .version(BUILD_VERSION)
-  .env(
-    'CHROMA_PROFILE=<PROFILE:string>',
-    `
-      Specifies the Chrome profile directory to use.
-      If -p/--profile is not specified, and this is set, this value will be used.
-    `,
-    { prefix: 'CHROMA_' },
-  )
-  .env(
-    'CHROMA_HOST=<HOST:string>',
-    `
-      Daemon socket to connect to.
-      If -H/--host is not specified, and this is set, this value will be used.
-    `,
-    { prefix: 'CHROMA_' },
-  )
-  .option(
-    '-p, --profile <PROFILE:string>',
-    `
-      Specify the profile to use.
-      If given value is an alias defined in the configuration file, it will be resolved to the corresponding profile directory name.
-    `,
-  )
-  .option('-c, --config <PATH:string>', 'Path to the configuration file.')
-  .option('-H, --host <HOST:string>', 'Daemon socket to connect to.')
-  .arguments('[ARGS...]')
-  .example('Specified profile by option.', 'chroma -p "Profile 2" http://localhost:5173')
-  .example('Specified profile by environment variable.', 'CHROMA_PROFILE="Profile 2" chroma http://localhost:5173')
-  .example(
-    'Use with other CLI tools via $BROWSER.',
-    `
-      export BROWSER="chroma" CHROMA_PROFILE="Profile 2"
-      gh issue list --web
-    `,
-  )
-  .parse()
+;(async () => {
+  const {
+    args: parsedArgs,
+    literal: parsedLiteral,
+    options: parsedOpts,
+  } = await new Command()
+    .name('chroma')
+    .description('Open URL in specific Google Chrome profile.')
+    .version(BUILD_VERSION)
+    .env(
+      'CHROMA_PROFILE=<PROFILE:string>',
+      `
+        Specifies the Chrome profile directory to use.
+        If -p/--profile is not specified, and this is set, this value will be used.
+      `,
+      { prefix: 'CHROMA_' },
+    )
+    .env(
+      'CHROMA_HOST=<HOST:string>',
+      `
+        Daemon socket to connect to.
+        If -H/--host is not specified, and this is set, this value will be used.
+      `,
+      { prefix: 'CHROMA_' },
+    )
+    .option(
+      '-p, --profile <PROFILE:string>',
+      `
+        Specify the profile to use.
+        If given value is an alias defined in the configuration file, it will be resolved to the corresponding profile directory name.
+      `,
+    )
+    .option('-c, --config <PATH:string>', 'Path to the configuration file.')
+    .option('-H, --host <HOST:string>', 'Daemon socket to connect to.')
+    .arguments('[ARGS...]')
+    .example('Specified profile by option.', 'chroma -p "Profile 2" http://localhost:5173')
+    .example('Specified profile by environment variable.', 'CHROMA_PROFILE="Profile 2" chroma http://localhost:5173')
+    .example(
+      'Use with other CLI tools via $BROWSER.',
+      `
+        export BROWSER="chroma" CHROMA_PROFILE="Profile 2"
+        gh issue list --web
+      `,
+    )
+    .parse()
 
-const program = Effect.flatMap(ChromeService, ($) =>
-  $.launch(O.fromNullable(parsedOpts.profile), [...parsedArgs, ...parsedLiteral]),
-)
+  const program = Effect.flatMap(ChromeService, ($) =>
+    $.launch(O.fromNullable(parsedOpts.profile), [...parsedArgs, ...parsedLiteral]),
+  )
 
-const MainLive = ChromeService.Default.pipe(
-  L.provide(Config.Default({ path: parsedOpts.config })),
-  L.provide(ChromeClient.Default({ socketPath: parsedOpts.host })),
-  L.provide(BunContext.layer),
-)
+  const MainLive = ChromeService.Default.pipe(
+    L.provide(Config.Default({ path: parsedOpts.config })),
+    L.provide(ChromeClient.Default({ socketPath: parsedOpts.host })),
+    L.provide(BunContext.layer),
+  )
 
-BunRuntime.runMain(program.pipe(Effect.provide(MainLive)))
+  BunRuntime.runMain(program.pipe(Effect.provide(MainLive)))
+})()
